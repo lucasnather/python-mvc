337cbdd915375123010b0ca0cc93c2c3b319060c
lucasnather <natherlucas@gmail.com>
2024-12-08 11:48:34 -0400

    feat: create entities

--- src/model/entities/juridical_person.py
+++ src/model/entities/juridical_person.py
@@ -1,5 +1,27 @@
+from sqlalchemy import Column, Integer, Float, TEXT
 from src.model.settings.base import Base
 
 class JuridicalPerson(Base):
     
     __tablename__ = "juridical_person"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    revenue = Column(Float)
+    age = Column(Integer)
+    dba = Column(TEXT)
+    phone = Column(TEXT, unique=True)
+    corporative_email = Column(TEXT, unique=True)
+    category = Column(TEXT)
+    balance = Column(Float)
+
+    def __repr__(self) -> str:
+        to_dict = {
+            "revenue", self.revenue,
+            "age", self.age,
+            "dba", self.dba,
+            "phone", self.phone,
+            "corporative_email", self.corporative_email,
+            "category", self.category,
+            "balance", self.balance,
+        }
+
+        return str(to_dict)
--- src/model/entities/natural_person.py
+++ src/model/entities/natural_person.py
@@ -1,6 +1,27 @@
+from sqlalchemy import Column, Integer, Float, TEXT
 from src.model.settings.base import Base
 
 class NaturalPerson(Base):
     
-    def __init__(self) -> None:
-        pass
+    __tablename__ = "natural_person"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    monthly_income = Column(Float)
+    age = Column(Integer)
+    fullname = Column(TEXT)
+    phone = Column(TEXT, unique=True)
+    email = Column(TEXT, unique=True)
+    category = Column(TEXT)
+    balance = Column(Float)
+
+    def __repr__(self) -> str:
+        to_dict = {
+            "monthly_income", self.monthly_income,
+            "age", self.age,
+            "fullname", self.fullname,
+            "phone", self.phone,
+            "email", self.email,
+            "category", self.category,
+            "balance", self.balance,
+        }
+
+        return str(to_dict)
