4df3ea91d965b28470b7cd89e7cdebcd93c388ab
lucasnather <natherlucas@gmail.com>
2024-12-09 16:48:25 -0400

    fix: change db_connection to connection

--- src/model/repositories/natural_person_repository.py
+++ src/model/repositories/natural_person_repository.py
@@ -5,12 +5,12 @@ from src.model.settings.connection import db_connection as DbConnection
 
 class NaturalPersonRepository(NaturalPersonInterface):
 
-    def __init__(self, db_connection: DbConnection) -> None:
-        self.db_connection = db_connection
+    def __init__(self, connection: DbConnection) -> None:
+        self.connection = connection
 
     
     def create(self,monthly_income: float, age: int, fullname: str, phone: str, email: str, category: str, balance: float):
-        with self.db_connection as database:
+        with self.connection as database:
             try:
                 natural_person = NaturalPerson(monthly_income, age, fullname, phone, email, category, balance)
                 database.session.add(natural_person)
@@ -21,7 +21,7 @@ class NaturalPersonRepository(NaturalPersonInterface):
                 raise exception
 
     def list_persons(self) -> List[NaturalPerson]:
-        with self.db_connection as database:
+        with self.connection as database:
             try:
                 natural_person = database.session.query(NaturalPerson).all()
                 return natural_person
@@ -30,7 +30,7 @@ class NaturalPersonRepository(NaturalPersonInterface):
         
 
     def list_person_by_id(self,person_id) -> NaturalPerson:
-        with self.db_connection as database:
+        with self.connection as database:
             try:
                 natural_person_by_id = database.session.query(NaturalPerson).filter(id=person_id)
                 return natural_person_by_id
@@ -39,12 +39,15 @@ class NaturalPersonRepository(NaturalPersonInterface):
         
 
     def transaction(self,natural_person_id,money) -> NaturalPerson:
-        with self.db_connection as database:
+        with self.connection as database:
             try:
                 natural_person = database.session.query(NaturalPerson).filter(id=natural_person_id)
                 natural_person.balance = money
+                database.session.add(natural_person)
+                database.sesstion.commit()
                 return natural_person
             except Exception as exception:
+                database.session.rollback()
                 raise exception
 
         
