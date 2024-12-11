b39da079831304dfb3ae44a42b0f078c008ef8d3
lucasnather <natherlucas@gmail.com>
2024-12-11 14:13:12 -0400

    feat: create repositories methods

--- src/model/interfaces/juridical_person_interface.py
+++ src/model/interfaces/juridical_person_interface.py
@@ -1,9 +1,18 @@
 from abc import abstractmethod, ABC
 from typing import Dict
+from src.model.entities.juridical_person import JuridicalPerson
 
 class JuridicalPersonInterface(ABC):
 
     @abstractmethod
     def create(self,revenue,age,dba, phone, corporative_email, category, balance) -> Dict:
+        pass
+
+    @abstractmethod
+    def list_person_by_id(self,juridical_person_id) -> JuridicalPerson:
+        pass
+
+    @abstractmethod
+    def transaction(self,juridical_person_id, money) -> JuridicalPerson:
         pass
         
--- src/model/repositories/juridical_person_repository.py
+++ src/model/repositories/juridical_person_repository.py
@@ -1,7 +1,7 @@
 from typing import Dict
 from src.model.interfaces.juridical_person_interface import JuridicalPersonInterface
-from src.model.entities.juridical_person import JuridicalPerson
 from src.model.settings.connection import db_connection as DbConnection
+from src.model.entities.juridical_person import JuridicalPerson
 
 class JuridicalPersonRepository(JuridicalPersonInterface):
     def __init__(self, connection: DbConnection) -> None:
@@ -23,4 +23,24 @@ class JuridicalPersonRepository(JuridicalPersonInterface):
                 database.session.commit()
                 return juridical_person
             except Exception as exception:
+                raise exception
+    
+    def list_person_by_id(self,juridical_person_id) -> JuridicalPerson:
+        with self.connection as database:
+            try:
+                juridical_person_by_id = database.session.query(JuridicalPerson).filter(JuridicalPerson.id == juridical_person_id).first()
+                return juridical_person_by_id
+            except Exception as exception:
+                raise exception
+            
+    def transaction(self,juridical_person_id,money) -> JuridicalPerson:
+        with self.connection as database:
+            try:
+                juridical_person = database.session.query(JuridicalPerson).filter(JuridicalPerson.id == juridical_person_id).first()
+                juridical_person.balance = money
+                database.session.add(juridical_person)
+                database.session.commit()
+                return juridical_person
+            except Exception as exception:
+                database.session.rollback()
                 raise exception
